9ae68e909b00d03dd5dd9cf518c8651961cf79af
ZowieKMK <schudo.business@gmail.com>
2024-08-07 12:20:06 +0700

    Fixed Microphone in PUBG Mobile

--- Costumized.bat
+++ Costumized.bat
@@ -1,4 +1,4 @@
 @echo off
 :start
-scrcpy --render-driver=OpenGL --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --stay-awake --max-fps=65 --video-bit-rate=60M -f
+scrcpy --render-driver=OpenGL --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --stay-awake --max-fps=65 --video-bit-rate=60M -f scrcpy --audio-source=playback  
 goto start

87dd40d43e44b1c7a4abf8ff0de04fa2dcafc90b
ZowieKMK <schudo.business@gmail.com>
2024-08-07 12:09:14 +0700

    fist commit SCRPY V2.6.1

--- Costumized_Simple.bat
+++ Costumized_Simple.bat
@@ -1,4 +1,4 @@
 @echo off
 :start
-scrcpy --render-driver=OpenGL --video-bit-rate=60M --video-codec=h264 --audio-source=playback --audio-source=playback --max-fps=65 --stay-awake -f
+scrcpy --render-driver=OpenGL --video-bit-rate=60M --video-codec=h264 --audio-source=playback --max-fps=65 --stay-awake -f
 goto start

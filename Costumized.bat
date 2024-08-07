49ec313818116006cdb5b61c78cdbef3ae6a47a8
ZowieKMK <schudo.business@gmail.com>
2024-08-07 12:05:35 +0700

    fixed Microphone by adding --audio-source=playback

--- Costumized.bat
+++ Costumized.bat
@@ -1,4 +1,4 @@
 @echo off
 :start
-scrcpy --render-driver=OpenGL --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --stay-awake --max-fps=65 --video-bit-rate=60M -f
+scrcpy --render-driver=OpenGL --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --audio-source=playback  --stay-awake --max-fps=65 --video-bit-rate=60M -f
 goto start

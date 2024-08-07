f28c1de5855430529fef38b90120b8e8263d5da4
ZowieKMK <schudo.business@gmail.com>
2024-08-07 20:42:02 +0700

    added costumized batch file

--- Costumized.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:start
-scrcpy --render-driver=OpenGL --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --stay-awake --max-fps=65 --video-bit-rate=60M -f --audio-source=playback  
-goto start

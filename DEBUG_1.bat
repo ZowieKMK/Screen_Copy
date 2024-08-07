1b1c82b590078764b1232b819b36b380e693e094
ZowieKMK <schudo.business@gmail.com>
2024-08-07 10:03:02 +0700

    cleaning files

--- DEBUG_1.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:start
-scrcpy --render-driver=OpenGL --max-fps=65 --video-bit-rate=60M -f --video-codec=h264 --video-encoder='OMX.MTK.VIDEO.ENCODER.AVC' --audio-codec=opus --audio-encoder='c2.android.opus.encoder' --stay-awake --audio-source=playback
-goto start

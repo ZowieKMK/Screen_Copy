66a646b764a73bdfe5061e24197955e7c5829952
ZowieKMK <schudo.business@gmail.com>
2024-08-08 10:10:19 +0700

    First release based on SCRCPY V2.6.1

--- RUN_SCRCPY.bat
+++ RUN_SCRCPY.bat
@@ -1,4 +1,4 @@
 @echo off
 :start
-scrcpy --render-driver=OpenGL --video-bit-rate=60M --video-codec=h264 --max-fps=65 --stay-awake -f --audio-source=playback --audio-source=playback 
+scrcpy --render-driver=OpenGL --video-bit-rate=60M --video-codec=h264 --max-fps=65 --stay-awake -f
 goto start
